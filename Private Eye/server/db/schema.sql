2ee97ff1f3d556ce401baf4ec78b790a8db3aea3
Ajthompson88 <Aj.thompson8888@gmail.com>
2025-03-06 18:53:41 -0500

    made some updates to the schema with a table added

--- Private Eye/server/db/schema.sql
+++ Private Eye/server/db/schema.sql
@@ -1,2 +1,12 @@
 DROP DATABASE IF EXISTS privateeye_db;
+
 CREATE DATABASE privateeye_db;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
